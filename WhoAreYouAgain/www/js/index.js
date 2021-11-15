88eaef47c9afe5071ea5c03906246656f46bf5f6
Damian Demiraj <damian.demiraj@buhlergroup.com>
2021-11-15 15:37:42 +0100

    Initial commit

---
+++ WhoAreYouAgain/www/js/index.js
@@ -0,0 +1,9 @@
+// Global
+// var test = 'aaaabc';
+
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {  // Document and device is ready
+    // Initial
+    $('main').load("../page.html")
+}
