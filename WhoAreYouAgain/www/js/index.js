6c6e2b4df335f187408c3247c81da6fc425d87b3
Damian Demiraj <damian.demiraj@buhlergroup.com>
2021-11-16 08:39:51 +0100

    Vibration working

--- WhoAreYouAgain/www/js/index.js
+++ WhoAreYouAgain/www/js/index.js
@@ -1,9 +1,13 @@
 // Global
-// var test = 'aaaabc';
+// var globalvar = 'aaaabc';
 
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {  // Document and device is ready
-    // Initial
-    $('main').load("../page.html")
+    $('main').load("../page.html");
+    document.getElementById("main").addEventListener("click", vibrate, true);
+}
+
+function vibrate() {
+    navigator.vibrate(5000);
 }
