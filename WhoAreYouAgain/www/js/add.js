91a17e23606739bdd80ebc4a6e3c2d29acde895f
Damian Demiraj <damian.demiraj@buhlergroup.com>
2021-11-23 08:38:45 +0100

    smaller adjustments

--- WhoAreYouAgain/www/js/add.js
+++ WhoAreYouAgain/www/js/add.js
@@ -2,16 +2,14 @@ var selectet;
 var ref = firebase.database().ref();
 
 $(".insert").click(function () {
-    // console.log($('#inputForm').children('#firstname'));
-    // console.log($('form').find('#inputForm').val());
-    // console.log($('#inputForm').find('#firstname').val);
+    console.log($('#inputForm').find('#picture').val);
     var ref = firebase.database().ref('user/');
     ref.push({
         "birthday":     $('#inputForm').find('#birthday').val(),
         "contact":      $('#inputForm').find('#contact').val(),
         "firstname":    $('#inputForm').find('#firstname').val(),
         "name":         $('#inputForm').find('#name').val(),
-        "picture":      $('#inputForm').find('#picture').val(),
+        "picture":      $('#inputForm').find('#picture').attr('src'),
         "remarks":      $('#inputForm').find('#remarks').val(),
     })
 
@@ -39,37 +37,17 @@ $(document).ready(function () {
     function onDeviceReady() {
       destinationType = navigator.camera.DestinationType;
     };
-    alert();
     $(".picture").click(function () {
-      alert("hey, id like a picture!");
         navigator.camera.getPicture(success, onFail, {
             quality: 50, destinationType: destinationType.DATA_URL
         });
     });
-    // $("#loeschen").click(function () {
-    //     $('#bild').css({
-    //         'display': "none"
-    //     });
-    //     $("#meldung").html('Bildanzeige gelöscht');
-    // });
-    // $("#speichern").click(function () {
-    //     navigator.camera.getPicture(onSuccess2, onFail, {
-    //         quality: 50, destinationType: destinationType.FILE_URI,
-    //         saveToPhotoAlbum: true,
-    //         encodingType: navigator.camera.EncodingType.PNG
-    //     });
-    // });
     function success(imageData) {
-        success.log(imageData);
         width = (screen.width * 0.9) + "px";
-        $('#bild').attr('src', "data:image/jpeg;base64," + imageData);
-        $('#bild').css({
+        $('#inputForm').find('#picture').attr('src', "data:image/jpeg;base64," + imageData);
+        $('#inputForm').find('#picture').css({
             'display': "block", 'width': width, 'margin': 'auto'
         });
-        var ref = firebase.database().ref('user/' + id);
-        ref.update({
-          "picture": "data:image/jpeg;base64," + imageData
-        })
     }
     function onFail(message) {
         $('#bild').css({
--- WhoAreYouAgain/www/js/home.js
+++ WhoAreYouAgain/www/js/home.js
@@ -2,38 +2,37 @@ var selectet;
 var ref = firebase.database().ref();
 
 ref.on("value", function (snapshot) {
-  console.log(snapshot.val());
   data = snapshot.val();
 
   var template = Handlebars.compile(`
         {{#each user}}
           <li id="{{@key}}">
-            <div class="singlePerson valign-wrapper collapsible-header"> {{name}}, {{firstname}}</div> 
+            <div class="singlePerson valign-wrapper collapsible-header"> {{name}} {{firstname}}</div> 
             <div class="collapsible-body"> 
 
               <div class="input-field">
                 <label class="active" for="firstname">Vorname</label>
-                <input placeholder="Vorname" id="firstname" type="text" class="validate edit" value={{firstname}}>
+                <input placeholder="Vorname" id="firstname" type="text" class="validate edit" value="{{firstname}}">
               </div>
               <div class="input-field">
                 <label class="active" for="name">Name</label>
-                <input placeholder="Name" id="name" type="text" class="validate edit" value={{name}}>
+                <input placeholder="Name" id="name" type="text" class="validate edit" value="{{name}}">
               </div>
               <div class="input-field">
                 <label class="active" for="contact">Kontakt</label>
-                <input placeholder="Kontakt" id="contact" type="text" class="validate edit" value={{contact}}>
+                <input placeholder="Kontakt" id="contact" type="text" class="validate edit" value="{{contact}}">
               </div>
               <div class="input-field">
                 <label class="active" for="birthday">Geburtstag</label>
-                <input placeholder="Geburtstag" id="birthday" type="date" class="validate edit" value={{birthday}}>
+                <input placeholder="Geburtstag" id="birthday" type="date" class="validate edit" value="{{birthday}}">
               </div>
               <div class="input-field">
                 <label class="active" for="remarks">Bemerkungen</label>
-                <input placeholder="Bemerkungen" id="remarks" type="text" class="validate edit" value={{remarks}}>
+                <input placeholder="Bemerkungen" id="remarks" type="text" class="validate edit" value="{{remarks}}">
               </div>
-              <div class="input-field">
+              <div class="picture input-field">
                 <label class="active" for="picture">Bild</label>
-                <img src="{{picture}}" alt="No picture available">
+                <img src="{{picture}}" id="picture" class="edit" type="picture" alt="No picture available">
               </div>
             </div>
           </li>
@@ -42,26 +41,73 @@ ref.on("value", function (snapshot) {
           selectet = $(this).parent().attr('id')
           $('#modal1').modal('open');
         }); </script>
+
+        
       `);
+
   var compiledTemplate = template(data);
   $("#out").html(compiledTemplate);
 
   $(".edit").focusout(function () {
     var attribute = $(this).attr("id").toString();
-    var value     = $(this).val();
-    var id        = $(this).parent().parent().parent().attr("id");
-    var ref = firebase.database().ref('user/' + id);
+    var value = $(this).val();
+    var id = $(this).parent().parent().parent().attr("id");
+    var ref = firebase.database().ref("user/" + id);
     ref.update({
-      [attribute]: [value]
-    })
-    // $(this).parent().siblings('.collapsible-header').addClass("active");
+      [attribute]: [value],
+    });
   });
+
   $(".delete").click(function () {
-    console.log(selectet)
-    console.log(ref)
-    firebase.database().ref("user/" + selectet).remove();
+    firebase
+      .database()
+      .ref("user/" + selectet)
+      .remove();
     vibrate();
   });
+
+  //////////////////// Camera /////////////////////////
+
+  (function () {
+    "use strict";
+
+    document.addEventListener("deviceready", onDeviceReady.bind(this), false);
+
+    function onDeviceReady() {
+      destinationType = navigator.camera.DestinationType;
+    }
+
+    $(".picture").click(function () {
+      // console.log("a");
+      var id = $(this).parent(id).parent().attr("id");
+      navigator.camera.getPicture(function (imageData) {
+        success(imageData, id)
+      }, onFail, {
+        quality: 50,
+        destinationType: destinationType.DATA_URL,
+      });
+    });
+    function success(imageData, id) {
+      console.log(id)
+      console.log(imageData)
+      // var id = $(this).parent().parent().parent().attr("id");
+      width = screen.width * 0.9 + "px";
+      // $(".picture")
+      //   .find("#picture")
+      //   .attr("src", "data:image/jpeg;base64," + imageData);
+      // var id = $(this).attr("id");
+      // console.log(id);
+      var ref = firebase.database().ref("user/" + id);
+      ref.update({
+        picture: "data:image/jpeg;base64," + imageData,
+      });
+    }
+    function onFail(message) {
+      $("#bild").css({
+        display: "none",
+      });
+    }
+  })();
 });
 
 $(document).ready(function () {
